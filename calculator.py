1fd34823da97aa368cbb40f819e4580fe02bdfc4
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 15:22:25 -0700

    added sum function to calculator.py

---
+++ calculator.py
@@ -0,0 +1,26 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+# # No setup
+# repeat forever:
+#     read input
+#     tokenize input
+#     if the first token is "q":
+#         quit
+#     else:
+#         decide which math function to call based on first token
+
+
+# Your code goes here
+while True:
+    input_string = input("Type your calculator command \n")
+    tokens = input_string.split(" ")
+    if tokens[0] == "q":
+        break
+    elif tokens[0] == '+':
+        print(add(float(tokens[1]), float(tokens[2])))
