cb03bfd88587c1a73076b5b1bc0a63701aef3ed6
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 16:13:02 -0700

    added square and cube functions to calculator.py

--- calculator.py
+++ calculator.py
@@ -17,15 +17,20 @@ from arithmetic import *
 
 
 # Your code goes here
+import functools
+
 while True:
     input_string = input('Type your calculator command \n')
     tokens = input_string.split(' ')
-    
+    # numbers_string = tokens.slice(1, -1, 1)
+    # numbers_float = [float(i) for i in numbers_string]
+
     if tokens[0] == 'q':
         break
 
     elif tokens[0] == '+':
         print(add(float(tokens[1]), float(tokens[2])))
+        # print (functools.reduce(lambda x,y: x + y, numbers_float))
 
     elif tokens[0] == '-':
         print(subtract(float(tokens[1]), float(tokens[2])))
@@ -38,3 +43,9 @@ while True:
             input_string = input('Second number cannot be 0. Try again. \nType your calculator command \n')
             tokens = input_string.split(' ')
         print(divide(float(tokens[1]), float(tokens[2])))
+
+    elif tokens[0] == "square":
+        print(square(float(tokens[1])))
+
+    elif tokens[0] == "cube":
+        print(cube(float(tokens[1])))
