aed36f3bf6bb7111b2652d51d62dbf17d19f0967
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 16:44:50 -0700

    Added divide and power functions to arithmetic1.py

--- arithmetic1.py
+++ arithmetic1.py
@@ -23,33 +23,20 @@ def multiply(list_nums):
     return mlt
 
 
-def divide(num1, num2):
+def divide(list_nums):
     """Divide the first input by the second, returning a floating point."""
+    div = list_nums[0]
+    for num in list_nums[1:]:
+        div /= num
+    return div
 
-    return num1 / num2
-
-
-def square(num1):
-    """Return the square of the input."""
-
-    # Needs only one argument
-
-    return num1 * num1
-
-
-def cube(num1):
-    """Return the cube of the input."""
-
-    # Needs only one argument
-
-    return num1 * num1 * num1
-
-
-def power(num1, num2):
+def power(list_nums):
     """Raise num1 to the power of num and return the value."""
-
-    return num1 ** num2  # ** = exponent operator
-
+    pwr = list_nums[0]
+    for num in list_nums[1:]:
+        pwr **= num
+    return pwr
+    
 
 def mod(num1, num2):
     """Return the remainder of num / num2."""
