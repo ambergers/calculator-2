2ca625e0e04f5220edf15cad802e0d171b8144b8
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 17:30:15 -0700

    Modified calculator.py to take in a list

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ Using the arithmetic.py file from Calculator Part 1, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import *
+from arithmetic1 import *
 
 # # No setup
 # repeat forever:
@@ -22,36 +22,31 @@ import functools
 while True:
     input_string = input('Type your calculator command \n')
     tokens = input_string.split(' ')
-    # numbers_string = tokens.slice(1, -1, 1)
-    # numbers_float = [float(i) for i in numbers_string]
+    num_list = [float(i) for i in tokens[1:]]
 
     if tokens[0] == 'q':
         break
 
     elif tokens[0] == '+':
-        print(add(float(tokens[1]), float(tokens[2])))
+        print(add(num_list))
         # print (functools.reduce(lambda x,y: x + y, numbers_float))
 
     elif tokens[0] == '-':
-        print(subtract(float(tokens[1]), float(tokens[2])))
+        print(subtract(num_list))
 
     elif tokens[0] == '*':
-        print(multiply(float(tokens[1]), float(tokens[2])))
+        print(multiply(num_list))
 
     elif tokens[0] == '/':
-        while tokens[2] == '0':
-            input_string = input('Second number cannot be 0. Try again. \nType your calculator command \n')
+        while '0' in tokens[2:]:
+            input_string = input('Only the first list item can be 0. Try again. \nType your calculator command \n')
             tokens = input_string.split(' ')
-        print(divide(float(tokens[1]), float(tokens[2])))
+            num_list = [float(i) for i in tokens[1:]]
 
-    elif tokens[0] == 'square':
-        print(square(float(tokens[1])))
-
-    elif tokens[0] == 'cube':
-        print(cube(float(tokens[1])))
+        print(divide(num_list))
 
     elif tokens[0] == 'power':
-        print(power(float(tokens[1]), float(tokens[2])))
+        print(power(num_list))
 
     elif tokens[0] == 'mod':
-        print(mod(float(tokens[1]), float(tokens[2])))
+        print(mod(num_list))
